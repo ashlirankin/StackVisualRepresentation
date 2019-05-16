03163724d37190dfbe020de1d02beb6554ca669c
Ashlirankin18 <ashlirankin18@gmail.com>
2019-05-15 20:57:48 -0400

    implemented the stack Data Structure

--- StackVisualRep.playground/Sources/StackImplementaion.swift
+++ StackVisualRep.playground/Sources/StackImplementaion.swift
@@ -1,2 +1,23 @@
 import Foundation
 
+public struct Stack<T>{
+  
+  var container = [T]()
+  
+  public init (){}
+  
+  var isEmpty:Bool {
+    return container.isEmpty
+  }
+  
+  var peek:T? {
+    return container.last
+  }
+  
+  mutating func push(value:T){
+    container.append(value)
+  }
+  mutating func pop() -> T? {
+    return container.popLast()
+  }
+}
