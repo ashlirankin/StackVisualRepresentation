a2a0dffdd0d60ee4cb1d1f3177fc9136e805f4f1
Ashlirankin18 <ashlirankin18@gmail.com>
2019-05-15 20:59:36 -0400

    commiting changes made to the playground file

--- StackVisualRep.playground/Contents.swift
+++ StackVisualRep.playground/Contents.swift
@@ -3,11 +3,33 @@ import PlaygroundSupport
 
 public class StackVisualViewController:UIViewController {
   
+  var stackCV = StackCV()
+    
   
   override public func viewDidLoad() {
     super.viewDidLoad()
-    
+    view.addSubview(stackCV)
+    view.backgroundColor = .orange
+    stackCV.stackCollectionView.dataSource = self
+    stackCV.stackCollectionView.delegate = self
   }
   
-
+ 
 }
+extension StackVisualViewController:UICollectionViewDataSource{
+  public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    guard let cell = stackCV.stackCollectionView.dequeueReusableCell(withReuseIdentifier: "StackCell", for: indexPath) as? StackCell else {fatalError("No StackCell found")}
+    cell.backgroundColor = .blue
+    return cell
+  }
+  public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 1
+  }
+}
+extension StackVisualViewController:UICollectionViewDelegateFlowLayout{
+  public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    return CGSize(width: 20, height: 20)
+  }
+}
+let viewController = StackVisualViewController()
+PlaygroundPage.current.liveView = viewController
