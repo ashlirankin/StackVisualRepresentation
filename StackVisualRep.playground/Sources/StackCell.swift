0077b9eb0ad7d5a5789a42efde05cc4058b55fd2
Ashlirankin18 <ashlirankin18@gmail.com>
2019-05-15 20:58:30 -0400

    created a custom collectionView  cell

--- StackVisualRep.playground/Sources/StackCell.swift
+++ StackVisualRep.playground/Sources/StackCell.swift
@@ -1,6 +1,46 @@
 import UIKit
-public class StackCell: UICollectionViewCell {
+
+public class StackCell: UICollectionViewCell{
   
+  let titleButton: UIButton = {
+    let button = UIButton()
+    button.setTitle("0", for: .normal)
+    button.setTitleColor(.black, for: .normal)
+    button.backgroundColor = .white
+    
+    return button
+  }()
+  public override init(frame: CGRect) {
+    super.init(frame: frame)
+    commonInit()
+  }
+  
+  public required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+    commonInit()
+  }
+  
+  func commonInit(){
+    
+    setUpViews()
+  }
   
 }
-
+extension StackCell {
+  func setUpViews(){
+    setupTitleButton()
+  }
+  
+  func setupTitleButton(){
+    addSubview(titleButton)
+    titleButton.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+        titleButton.centerXAnchor.constraint(equalTo: centerXAnchor),
+      titleButton.centerYAnchor.constraint(equalTo: centerYAnchor),
+      titleButton.heightAnchor.constraint(equalToConstant: frame.height/2),
+      titleButton.widthAnchor.constraint(equalToConstant: frame.width/2)
+      
+      ])
+  
+  }
+}
